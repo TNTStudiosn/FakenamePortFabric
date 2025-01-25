958fbf340aaaced167ecb8742375a2efa5217df6
TNTStudiosn <tntstudiosn@gmail.com>
2025-01-25 02:44:40 -0600

    Initial commit

---
+++ src/client/java/org/TNTStudios/fakenameportfabric/client/FakenameportfabricClient.java
@@ -0,0 +1,10 @@
+package org.TNTStudios.fakenameportfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FakenameportfabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/TNTStudios/fakenameportfabric/Fakenameportfabric.java
@@ -0,0 +1,10 @@
+package org.TNTStudios.fakenameportfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fakenameportfabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
